97cdc92eaa0aa95e152e468826d8372a6ec8114b
Tom Drew <tomdrew@Toms-MacBook-Air.local>
2024-11-12 21:33:24 +0000

    added datetime

--- carsharing.py
+++ carsharing.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,8 @@ app = FastAPI()
 def welcome():
     """return a welcome message"""
     return {'message': "Welcome to the Car Sharing service!"}
+
+@app.get("/date")
+def welcome():
+    """return the current date and time"""
+    return {'date': datetime.now()}
